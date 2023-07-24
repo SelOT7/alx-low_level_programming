c6077740a0bd0b7dff29425a39fdd0b7b8ad3575
SelOT7 <selamawitsel2@gmail.com>
2023-07-24 16:32:36 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --output-document=$PWD/libtst.so https://github.com/Elmahdi1962/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib_inject.so?raw=true
+wget -q --output-document=$PWD/libtest.so https://github.com/SelOT7/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
 export LD_PRELOAD=$PWD/libtest.so
