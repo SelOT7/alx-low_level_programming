3c468dcd13711cbdafa0833c046d3fe66950b378
SelOT7 <selamawitsel2@gmail.com>
2023-07-24 17:23:21 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -q --output-document=$PWD/libtest.so https://github.com/SelOT7/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD=$PWD/libtest.so
